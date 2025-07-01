aab98a552764c595f3e6736a3dc843158b5eaa12
CiutacuLigia <ligia.ciutacu@yahoo.com>
2025-07-01 12:51:50 +0300

    Implementare calcul grupe de sange probabile

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.ConstrainedExecution;
+﻿using System.ComponentModel.Design;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.Intrinsics.X86;
 
 namespace GrupeDeSange
 {
@@ -7,28 +9,57 @@ namespace GrupeDeSange
     {
         static void Main(string[] args)
         {
-            Console.WriteLine( "Introdu grupa de sange pe care o ai");
-            string grupaIntrodusa = Console.ReadLine();
-            if ( grupaIntrodusa ==  "A" )
-        {
-            Console.WriteLine("Parintii cel mai probabil au grupele: \r\n\r\nA si A\r\n\r\nA si O\r\n\r\nA si AB\r\n\r\nAB si O\r\n\r\nB si A (dacă ambii sunt AO sau BO)");
-        }
-        else if ( grupaIntrodusa == "B" )
-        {
-            Console.WriteLine("Parintii cel mai probabil au grupele: \r\n\r\nB si B\r\n\r\nB si O\r\n\r\nB si AB\r\n\r\nA si B (dacă A este AO, iar B este BO)");
-        }
-        else if( grupaIntrodusa ==  "AB" )
-            {
-                Console.WriteLine("Parintii cel mai probabil au grupele \r\n\r\nA si B\r\n\r\nAB si A\r\n\r\nAB si B\r\n\r\nAB si AB ") ;
+            Console.WriteLine("Introdu grupa de sange pe care o ai pentru a va afisa posibilele genotipuri! Cat si grupa parintilor tai! \r\n Grupele de sange sunt A,B,AB sau 0\r\n");
+            string[,] genotip;
+            genotip = new string[,]   {    { "0", "La", "Lb", "l" },
+                                           { "La", "A", "AB", "A"},
+                                           { "Lb", "AB", "B", "B"},
+                                           { "l", "A", "B", "0"},
+                                          };
+            string[] fenotip;
+            fenotip = new string[] { " Grupa A", "Grupa B", "Grupa AB ", "Grupa 0" };
+            ;
+           
 
-            }
-            else if (grupaIntrodusa ==  "0" )
-            {
-                Console.WriteLine("Parintii cel mai probabil au grupele \r\n\r\nO si O\r\n\r\nA (AO) si O\r\n\r\nB (BO) si O\r\n\r\nA (AO) si B (BO)") ;
-            }
+            string valoareIntrodusa = Console.ReadLine();
+            if (valoareIntrodusa == genotip[1,1]) // pentru grupa de sange A
+            { Console.WriteLine($"Genotipul dumneavoastra probabil este {genotip[0, 1] + genotip[1,0]} sau {genotip[1,0] + genotip[0,3]} \r\n  grupa parintilor tai poate fi: \r\n ambii {fenotip[0]} \r\n unul {fenotip[0]} si altul {fenotip[1]} \r\n unul{fenotip[0]} si altul {fenotip[3]} \r\n unul{ fenotip[1]} si altul { fenotip[3]} "); }
+            else if (valoareIntrodusa == genotip[2,2]) //pentru grupa de sange B
+            { Console.WriteLine($"Genotipul dumneavoastra probabil este {genotip[0, 2] + genotip[2, 0]} sau {genotip[2, 0] + genotip[0, 3]} \r\n  grupa parintilor tai poate fi: \r\n ambii {fenotip[1]} \r\n unul {fenotip[1]} si altul {fenotip[3]} \r\n unul{fenotip[0]} si altul {fenotip[1]} "); }
+            else if (valoareIntrodusa == genotip[1,2]) //pentru grupa de sange AB
+            { Console.WriteLine($"Genotipul dumneavoastra cu siguranta este {genotip[1, 0] + genotip[0, 2]} \r\n grupa parintilor sigur este: \r\n unul {fenotip[0]} si altul {fenotip[1]} "); }
+            else if (valoareIntrodusa == genotip[3,3]) //pentru grupa de sange 0
+            { Console.WriteLine($"Genotipul dumneavoastra cu siguranta este {genotip[3, 0] + genotip[0, 3]} \r\n grupa parintilor cu siguranta consta in macar unu sa aiba grupa {fenotip[3]} "); }
+
+            
+            
+                                         
+                                     
+                             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
-        
 
     }
-    
 }
