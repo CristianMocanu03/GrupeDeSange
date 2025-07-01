fce8f458aec752966f44c1af6e5ca58c626b2aa7
CiutacuLigia <ligia.ciutacu@yahoo.com>
2025-07-01 13:11:13 +0300

    Varianta finala calcul grupe de sange

--- Program.cs
+++ Program.cs
@@ -9,33 +9,40 @@ namespace GrupeDeSange
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Introdu grupa de sange pe care o ai pentru a va afisa posibilele genotipuri! Cat si grupa parintilor tai! \r\n Grupele de sange sunt A,B,AB sau 0\r\n");
-            string[,] genotip;
-            genotip = new string[,]   {    { "0", "La", "Lb", "l" },
+            var valoareIntrodusa = string.Empty;
+             while (valoareIntrodusa != "C")
+            {
+ Console.WriteLine("Introdu grupa de sange pe care o ai pentru a va afisa posibilele genotipuri! Cat si grupa parintilor tai! \r\n Grupele de sange sunt A,B,AB sau 0\r\n");
+               
+           
+
+                string[,] genotip;
+                genotip = new string[,]   {    { "0", "La", "Lb", "l" },
                                            { "La", "A", "AB", "A"},
                                            { "Lb", "AB", "B", "B"},
                                            { "l", "A", "B", "0"},
                                           };
-            string[] fenotip;
-            fenotip = new string[] { " Grupa A", "Grupa B", "Grupa AB ", "Grupa 0" };
-            ;
+                string[] fenotip;
+                fenotip = new string[] { " Grupa A", "Grupa B", "Grupa AB ", "Grupa 0" };
+
+                ;
+
+                 valoareIntrodusa = Console.ReadLine();
+                if (valoareIntrodusa == genotip[1, 1]) // pentru grupa de sange A
+                { Console.WriteLine($"Genotipul dumneavoastra probabil este {genotip[0, 1] + genotip[1, 0]} sau {genotip[1, 0] + genotip[0, 3]} \r\n  grupa parintilor tai poate fi: \r\n ambii {fenotip[0]} \r\n unul {fenotip[0]} si altul {fenotip[1]} \r\n unul{fenotip[0]} si altul {fenotip[3]} \r\n unul{fenotip[1]} si altul {fenotip[3]} "); }
+                else if (valoareIntrodusa == genotip[2, 2]) //pentru grupa de sange B
+                { Console.WriteLine($"Genotipul dumneavoastra probabil este {genotip[0, 2] + genotip[2, 0]} sau {genotip[2, 0] + genotip[0, 3]} \r\n  grupa parintilor tai poate fi: \r\n ambii {fenotip[1]} \r\n unul {fenotip[1]} si altul {fenotip[3]} \r\n unul{fenotip[0]} si altul {fenotip[1]} "); }
+                else if (valoareIntrodusa == genotip[1, 2]) //pentru grupa de sange AB
+                { Console.WriteLine($"Genotipul dumneavoastra cu siguranta este {genotip[1, 0] + genotip[0, 2]} \r\n grupa parintilor sigur este: \r\n unul {fenotip[0]} si altul {fenotip[1]} "); }
+                else if (valoareIntrodusa == genotip[3, 3]) //pentru grupa de sange 0
+                { Console.WriteLine($"Genotipul dumneavoastra cu siguranta este {genotip[3, 0] + genotip[0, 3]} \r\n grupa parintilor cu siguranta consta in macar unu sa aiba grupa {fenotip[3]} "); }
+            }
            
 
-            string valoareIntrodusa = Console.ReadLine();
-            if (valoareIntrodusa == genotip[1,1]) // pentru grupa de sange A
-            { Console.WriteLine($"Genotipul dumneavoastra probabil este {genotip[0, 1] + genotip[1,0]} sau {genotip[1,0] + genotip[0,3]} \r\n  grupa parintilor tai poate fi: \r\n ambii {fenotip[0]} \r\n unul {fenotip[0]} si altul {fenotip[1]} \r\n unul{fenotip[0]} si altul {fenotip[3]} \r\n unul{ fenotip[1]} si altul { fenotip[3]} "); }
-            else if (valoareIntrodusa == genotip[2,2]) //pentru grupa de sange B
-            { Console.WriteLine($"Genotipul dumneavoastra probabil este {genotip[0, 2] + genotip[2, 0]} sau {genotip[2, 0] + genotip[0, 3]} \r\n  grupa parintilor tai poate fi: \r\n ambii {fenotip[1]} \r\n unul {fenotip[1]} si altul {fenotip[3]} \r\n unul{fenotip[0]} si altul {fenotip[1]} "); }
-            else if (valoareIntrodusa == genotip[1,2]) //pentru grupa de sange AB
-            { Console.WriteLine($"Genotipul dumneavoastra cu siguranta este {genotip[1, 0] + genotip[0, 2]} \r\n grupa parintilor sigur este: \r\n unul {fenotip[0]} si altul {fenotip[1]} "); }
-            else if (valoareIntrodusa == genotip[3,3]) //pentru grupa de sange 0
-            { Console.WriteLine($"Genotipul dumneavoastra cu siguranta este {genotip[3, 0] + genotip[0, 3]} \r\n grupa parintilor cu siguranta consta in macar unu sa aiba grupa {fenotip[3]} "); }
 
-            
-            
-                                         
-                                     
-                             
+
+
+
 
 
 
